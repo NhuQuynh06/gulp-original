5fd30222c59310cd29c6993e456548b28b9f2633
quynhttn3 <quynhttn3@vng.com.vn>
2023-03-13 11:36:53 +0700

    add modal, audio

--- dist/desktop/js/ui.js
+++ dist/desktop/js/ui.js
@@ -1,6 +1,18 @@
 "use strict";
 
-// check gif img
+// ----------------------------------
+// INFO : Js global
+// ----------------------------------
+
+// INFOR : for header
+var stickyOffset = $('.sticky').offset().top;
+$(window).scroll(function () {
+  var sticky = $('.sticky'),
+    scroll = $(window).scrollTop();
+  if (scroll >= stickyOffset) sticky.addClass('fixed');else sticky.removeClass('fixed');
+});
+
+// INFO : for check gif img
 var img = $(".story .story__thumb img");
 $.each(img, function () {
   var dataSrc = this.dataset.src;
@@ -8,3 +20,48 @@ $.each(img, function () {
     $(this).closest("figure").css("background", "#eee");
   }
 });
+
+// INFO : for audio
+$(".audio .right .select-voice").click(function (e) {
+  e.preventDefault();
+  $(".audio .right .voice-board").toggle();
+});
+$(".audio .play-button").click(function (e) {
+  e.preventDefault();
+  $(".audio .ic-audio-pause").toggleClass("ic-audio-play");
+});
+$(".audio .mute-button").click(function (e) {
+  e.preventDefault();
+  $(".audio .ic-audio-volume").toggleClass("ic-audio-volume-slash");
+});
+$(".modal-body .ic-eye").click(function (e) {
+  e.preventDefault();
+  $(this).toggleClass("slash");
+});
+
+// INFO : for modal
+$("body").append("<div class='modal-backdrop'></div>");
+$('.btn-modal').on('click', function () {
+  $("body").addClass("modal-open");
+  $('.modal-backdrop').fadeIn();
+  var id = $(this).data('target');
+  $('.modal[data-target="' + id + '"]').fadeIn();
+});
+$('.btn-close').on('click', function () {
+  setTimeout(function () {
+    $("body").removeClass("modal-open");
+  }, 400);
+  $('.modal-backdrop').fadeOut();
+  $('.modal').fadeOut();
+});
+$('.modal-backdrop').on('click', function () {
+  setTimeout(function () {
+    $("body").removeClass("modal-open");
+  }, 400);
+  $('.modal-backdrop').fadeOut();
+  $('.modal').fadeOut();
+});
+
+// ----------------------------------
+// INFO : Js for project
+// ----------------------------------
