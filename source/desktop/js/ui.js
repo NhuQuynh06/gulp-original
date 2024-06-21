556e6beedec606e26432cc7b5186b6bc2d74bab0
NhuQuynh06 <quynhtran0618@gmail.com>
2024-06-21 17:12:12 +0700

    style

---
+++ source/desktop/js/ui.js
@@ -0,0 +1,13 @@
+// swiper
+if ($(".article").length) {
+  var swiper = new Swiper(".article__slider", {
+    autoHeight: true,
+    loop: true,
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
