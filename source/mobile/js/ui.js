861bdfa484a3baaa1954baaedf680fed91a8ac66
NhuQuynh06 <quynhtran0618@gmail.com>
2024-06-26 17:19:25 +0700

    style

--- source/mobile/js/ui.js
+++ source/mobile/js/ui.js
@@ -1,3 +1,35 @@
+// INFO : default for article
+// swiper
+if ($(".article").length) {
+  var swiper = new Swiper(".article__slider", {
+    autoHeight: true,
+    loop: true,
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
+
+// copy-source
+if ($(".article__source .getlink").length > 0) {
+  $(".article__source .btn").on("click", function () {
+    $(".article__source .source-toggle").toggleClass("show");
+  });
+
+  var $temp = $("<input>");
+  var $url = $(".article__source .source-toggle a").attr("href");
+  $(".article__source .getlink").on("click", function () {
+    $("body").append($temp);
+    $temp.val($url).select();
+    document.execCommand("copy");
+    $temp.remove();
+    $(".article__source .getlink").html("Đã copy");
+  });
+}
+
 // =============== js default ===================
 // INFOR : for header
 var showNavigation = $(".site-header .ic-menu");
@@ -72,70 +104,7 @@ $(".btn-close").on("click", function () {
   $(".modal").fadeOut();
 });
 
-// INFOR : for comment
-var input = $(".wrap-comment .input-comment");
-input.on("click", function () {
-  input.addClass("active");
-});
 
-// INFOR : slider
-if ($(".networks").length) {
-  const swiperNetworks = new Swiper(".networks .swiper", {
-    slidesPerView: "auto",
-    spaceBetween: 6,
-    loop: true,
-  });
-}
-
-if ($(".box-webstory").length) {
-  const swiperbWebStory = new Swiper(".box-webstory.swiper", {
-    slidesPerView: 1.5,
-    spaceBetween: 16,
-    loop: true,
-  });
-}
-if ($(".mega-story").length) {
-  const swiperMegaStory = new Swiper(".mega-story.swiper", {
-    slidesPerView: "auto",
-    spaceBetween: 16,
-    loop: true,
-  });
-}
-
-if ($(".short-video").length) {
-  const swiperShortVideo = new Swiper(".short-video.swiper", {
-    slidesPerView: "auto",
-    spaceBetween: 20,
-    loop: true,
-  });
-}
-
-if ($(".topics").length) {
-  const swiperTopics = new Swiper(".topics.swiper", {
-    slidesPerView: "auto",
-    spaceBetween: 20,
-    loop: true,
-  });
-}
-
-if ($(".podcast-series").length) {
-  const swiperPodcastSeries = new Swiper(".podcast-series.swiper", {
-    slidesPerView: "auto",
-    spaceBetween: 20,
-    loop: true,
-  });
-}
-
-// INFOR : check dark theme
-var body = $("body");
-if (
-  body.hasClass("media-page") ||
-  body.hasClass("video-page") ||
-  // body.hasClass("timeline-page") ||
-  body.hasClass("photo-page")
-) {
-  $("html").addClass("dark-theme");
-}
 // INFOR : back to top
 var btn = $(".back-to-top");
 $(window).scroll(function () {
@@ -150,71 +119,3 @@ btn.on("click", function (e) {
   e.preventDefault();
   $("html, body").animate({ scrollTop: 0 }, "300");
 });
-
-// INFO : region filter
-$(".search-filter .submit").click(function (e) {
-  e.preventDefault();
-  $(".search-filter .menu").fadeToggle("fast");
-});
-
-// INFO : show noti on header
-var showNoti = $(".site-header .ic-bell");
-showNoti.click(function (e) {
-  e.preventDefault();
-  $(".site-header .pick-news").fadeToggle("fast");
-});
-
-// INFO : short video detail
-var mySwiper = new Swiper(".mySwiper", {
-  loop: true,
-  direction: "vertical",
-});
-
-// short video
-if ($(".video-detail").length) {
-  let w = window.innerWidth;
-  $(".video-modal").css("max-width", w);
-}
-
-$(".wrapper-short-video .story").click(function (e) {
-  e.preventDefault();
-  setTimeout(() => {
-    if ($(".video-detail .swiper-slide-active").length) {
-      background();
-    }
-  }, 100);
-});
-
-// get img make background
-const background = function () {
-  let porter = $(".video-detail .swiper-slide-active .content video").attr(
-    "poster"
-  );
-  $(".video-detail .bg-blur").css(
-    "background",
-    "url(" + porter + ") center center/ auto 100% no-repeat"
-  );
-};
-
-mySwiper.on("slideChange", function (e) {
-  setTimeout(() => {
-    background();
-  }, 500);
-});
-
-// INFO copy source
-if ($(".article__source .getlink").length > 0) {
-  $(".article__source .btn").on("click", function () {
-    $(".article__source .source-toggle").toggleClass("show");
-  });
-
-  var $temp = $("<input>");
-  var $url = $(".article__source .source-toggle a").attr("href");
-  $(".article__source .getlink").on("click", function () {
-    $("body").append($temp);
-    $temp.val($url).select();
-    document.execCommand("copy");
-    $temp.remove();
-    $(".article__source .getlink").html("Đã copy");
-  });
-}
